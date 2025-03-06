60ae18f43f94caa9dfddbab17f4f726923a7c8f6
EdissonG <edisson.guauta@gmail.com>
2025-03-06 12:13:05 -0500

    Implementacion endpoint read para usuarios

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import 'dotenv/config.js'
 import './config/database.js'
+import cors from 'cors'
+import morgan from "morgan"
+import rounterIndex from "./router/index.js"
 
 const server = express()
 
@@ -8,11 +11,14 @@ const PORT = process.env.PORT || 8080
 
 const ready = ()=> console.log("Server ready in port :"+PORT);
 
-server.get("/",(request,response) => {
-    response.send('Hola, bienvenidos a EXPRESS')
-})
-server.get("/hola",(request,response) => {
-    response.send('Chao')
-})
+//configuraciones basicas
+server.use(express.urlencoded({extended:true}))
+server.use(express.json())
+server.use(cors())
+server.use(morgan('dev'))
+
+//Configurar Enrutador
+server.use('/api',rounterIndex)
+
 
 server.listen(PORT,ready)
